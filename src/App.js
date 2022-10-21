8cafd9b8c5da44ec61ead85776ccc7d217822215
Gaurav Tiwari <gtiwari037@gmail.com>
2022-10-21 22:04:27 +0530

    feat: react-query-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,163 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
+import {
+  useQuery,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
+  const [postId, setPostId] = React.useState(-1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <QueryClientProvider client={queryClient}>
+      <p>
+        As you visit the posts below, you will notice them in a loading state
+        the first time you load them. However, after you return to this list and
+        click on any posts you have already visited again, you will see them
+        load instantly and background refresh right before your eyes!{" "}
+        <strong>
+          (You may need to throttle your network speed to simulate longer
+          loading sequences)
+        </strong>
+      </p>
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
+}
+
+function usePosts(pageno) {
+  console.log(pageno,"test")
+  return useQuery(["posts",pageno], async () => {
+    const result = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageno}`
+    );
+    const data = await result.json()
+    return data;
+  });
+}
+
+function Posts({ setPostId }) {
+  const [pageno , setPageno] = useState(0)
+
+  const queryClient = useQueryClient();
+  const { status, data, error, isFetching } = usePosts(pageno);
+
+  const client = useQueryClient()
+
+
+//prefetch
+  useEffect(()=>{
+ const next = pageno +1
+ client.prefetchQuery((["posts",next], async () => {
+  const result = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${next}`
+    );
+  const data = await result.json()
+  return data;
+
+
+}))
+  },[pageno])
+
+  const handleclick = ()=>{
+    setPageno(pageno+1)
+  }
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>
+              {data.map((post) => (
+                <p key={post.id}>
+                  <a
+                    onClick={() => setPostId(post.id)}
+                    href="#"
+                    style={
+                      // We can access the query data here to show bold links for
+                      // ones that are cached
+                      queryClient.getQueryData(["post", post.id])
+                        ? {
+                            fontWeight: "bold",
+                            color: "green",
+                          }
+                        : {}
+                    }
+                  >
+                    {post.title}
+                  </a>
+                </p>
+              ))}
+            </div>
+            <div>{isFetching ? "Background Updating..." : " "}</div>
+            <button onClick={handleclick}>next</button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
 
-export default App;
+const getPostById = async (id) => {
+  const result = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  const data = await result.json()
+  return data;
+};
+
+function usePost(postId) {
+  return useQuery(["post", postId], () => getPostById(postId), {
+    enabled: !!postId,
+  });
+}
+
+function Post({ postId, setPostId }) {
+
+
+  const { status, data, error, isFetching } = usePost(postId);
+
+ 
+
+  return (
+    <div>
+      <div>
+        <a onClick={() => setPostId(-1)} href="#">
+          Back
+        </a>
+      </div>
+      {!postId || status === "loading" ? (
+        "Loading..."
+      ) : status === "error" ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <>
+          <h1>{data.title}</h1>
+          <div>
+            <p>{data.body}</p>
+          </div>
+          <div>{isFetching ? "Background Updating..." : " "}</div>
+         
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App
